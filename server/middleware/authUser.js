80fea588bfb2b7ff101af4f4ee94c3cc98f3837e
brf153 <153hsb@gmail.com>
2025-09-07 23:24:20 +0530

    fix auth issue
    Signed-off-by: brf153 <153hsb@gmail.com>

--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -2,8 +2,7 @@ import jwt from "jsonwebtoken"
 
 const authUser = async(req,res,next)=>{
     const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1]; 
-    console.log("token", token)
+    const token = authHeader && authHeader.split(" ")[1];
 
     if(!token){
         return res.json({success:false, message:"Not Authorized Login Again"})
