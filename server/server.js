3af00c20771c51e9bead7e453f7f78a8f37296df
Saptarshi Chakraborty <saptarshichakraborty783@gmail.com>
2025-09-07 21:19:45 +0530

    Frontend origin added

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB(); // Establish connection to the Database
 await connectCloudinary(); // Setup Cloudinary for image storage
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://e-commerce-website-beta-lake.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
