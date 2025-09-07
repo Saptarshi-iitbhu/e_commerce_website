859966af8e604b06fd4c688679773ca69fd3bd7a
Saptarshi Chakraborty <saptarshichakraborty783@gmail.com>
2025-09-07 14:23:57 +0530

    webhook route added

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authAdmin from "../middleware/authAdmin.js"
-import { allOrders, placeOrderCOD, placeOrderStripe, updateStatus, userOrders } from "../controllers/orderController.js"
+import { allOrders, placeOrderCOD, placeOrderStripe, stripeWebhooks, updateStatus, userOrders } from "../controllers/orderController.js"
 import authUser from "../middleware/authUser.js"
 
 const orderRouter = express.Router()
@@ -11,6 +11,7 @@ orderRouter.post('/status', authAdmin, updateStatus)
 //For Payment
 orderRouter.post('/cod', authUser, placeOrderCOD)
 orderRouter.post('/stripe', authUser, placeOrderStripe)
+orderRouter.post('/webhook', stripeWebhooks)
 // For User
 orderRouter.post('/userorders', authUser, userOrders)
 
